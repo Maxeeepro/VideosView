1fb58b8f242ca341164396904c60b37652e83d85
Maxeeepro <maksym.gaidai@gmail.com>
2019-10-05 20:45:09 +0300

    App has been updated

--- app/src/main/java/com/example/videosview/MainActivity.java
+++ app/src/main/java/com/example/videosview/MainActivity.java
@@ -1,117 +1,25 @@
 package com.example.videosview;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-
-import model.Movie;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
-import web.RecyclerInterface;
-import web.RetrofitAdapter;
-
-public class MainActivity extends AppCompatActivity {
-    private RetrofitAdapter retrofitAdapter;
-    private RecyclerView recyclerView;
-
+public class InitActivity extends AppCompatActivity implements View.OnClickListener {
+Button btnStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recycler);
-        recyclerView.setAdapter(retrofitAdapter);
-        Log.i("onCreate", "onCreate");
-        fetchJSON();
+        setContentView(R.layout.activity_init);
+        btnStart = (Button)findViewById(R.id.button2);
+
     }
-
-    //create retrofit builder
-    private void fetchJSON() {
-        Retrofit retrofit = new Retrofit.Builder()
-                //get base url from Recycler Interface
-                .baseUrl(RecyclerInterface.JSONURL)
-                .addConverterFactory(ScalarsConverterFactory.create())
-                .build();
-        //creation retrofit with recyclerInterface in parameters
-        RecyclerInterface api = retrofit.create(RecyclerInterface.class);
-
-        Call<String> call = api.getString();
-        //implementation of the Retrofit callback
-        call.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                //write some logs
-                Log.i("Responsestring", response.body().toString());
-                if (response.isSuccessful()) {
-                    if (response.body() != null) {
-                        Log.i("onSuccess", response.body().toString());
-                        //getting JSON response body
-                        String jsonresponse = response.body().toString();
-                        //put this body into the writeRecycler method like input parameters
-                        writeRecycler(jsonresponse);
-                    } else {
-                        Log.i("onEmptyResponse", "Returned empty response");
-                    }
-                }
-
-            }
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                Log.i("onEmptyResponse", "onFailure");
-                //if this error exist will print error
-                Log.i("onFailure", "error " + t.getMessage());
-            }
-        });
-    }
-    private void writeRecycler(String response) {
-        try {
-            ArrayList<Movie> modelRecyclerArraylist = new ArrayList<>();
-            //getting the whole json object from response
-            JSONObject obj = new JSONObject(response);
-            //create new array with JSON objects from response
-            //initiate JSONArray by name of JSONArray
-            JSONArray dataArray = obj.getJSONArray("categories");
-            //iteration on array with JSON objects
-            JSONObject jsonObject = dataArray.getJSONObject(0);
-            JSONArray jsonArray = jsonObject.getJSONArray("videos");
-//            .getJSONArray("categories");
-            for (int i = 0; i < jsonArray.length(); i++) {
-                //create an object of Model Class
-                Movie modelRecycler = new Movie();
-                //work with every element from this array
-                //
-                JSONObject dataobj = jsonArray.getJSONObject(i);
-                Log.i("JSONobjects",dataobj.toString());
-                //create object Movie type and setting objects from JSON to this object
-                modelRecycler.setImageLarge(dataobj.getString("image-780x1200"));
-                modelRecycler.setSubtitle(dataobj.getString("subtitle"));
-                modelRecycler.setStudio(dataobj.getString("studio"));
-                modelRecycler.setImageSmall(dataobj.getString("image-480x270"));
-                //add to the collection my model object
-                modelRecyclerArraylist.add(modelRecycler);
-                //creation new object of the Retrofit adapter
-                //this context and new ArrayList which were created earlier
-                retrofitAdapter = new RetrofitAdapter(this, modelRecyclerArraylist);
-                //find recycler view by ID in XML
-
-                //set to recyclerView new adapter
-                recyclerView.setAdapter(retrofitAdapter);
-                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+//On click method for starting Main Activity
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

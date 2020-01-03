b9eb600507ed353cd618ff797c1aef03bb402255
Volodymyr Fediuchko <volodymyr.fediuchko@hotschedules.com>
2020-01-03 14:57:51 +0300

    Revert "Revert "New branch with rx""

--- app/src/main/java/web/RetrofitCreateInstance.java
+++ app/src/main/java/web/RetrofitCreateInstance.java
@@ -1,16 +1,9 @@
 package web;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.lang.reflect.Type;
-
-import retrofit2.Converter;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitCreateInstance {
-    private static final String BASE_URL = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/";
+    public static final String BASE_URL = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(){
